da4bfd1c7761cae86ae1fc96835e6aae57c42a67
Arsonist1337 <micho129@wp.pl>
2020-03-11 18:44:48 +0100

    Drugi

---
+++ Zadania.py
@@ -0,0 +1,58 @@
+# Zadanie 1
+a = 1
+b = 2
+c = 3.14
+d = 5.26
+e = 8 + 17
+f = 5 + 3
+g = ' Ala ma kota'
+h = "string"
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+
+# Zadanie 2
+a = 4
+b = 2
+dzielenie = a / b
+print(dzielenie)
+mnozenie = a * b
+print(mnozenie)
+dodawanie = a + b
+print(dodawanie)
+odejmowanie = a - b
+print(odejmowanie)
+dzielenie_calk = a // b
+print(dzielenie_calk)
+reszta = a % b
+print(reszta)
+potega = a ** b
+print(potega)
+potega = pow(a,b)
+print(potega)
+
+# Zadanie 3
+
+a = 4
+a += 2
+print(a)
+a = 4
+a -= 2
+print(a)
+a = 4
+a *= 2
+print(a)
+a = 4
+a /= 2
+print(a)
+a = 4
+a **= 2
+print(a)
+a = 4
+a %= 2
+print(a)
